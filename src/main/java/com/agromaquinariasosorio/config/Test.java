9524402545e96d973e340de39333292a91aabaed
Alexander Fuentes Medina <luiguialexander.fuentesmedina@emeal.nttdata.com>
2025-06-12 13:25:04 -0500

    Metodos de registrar, actualizar, leer, listar, listar por idcategoria fueron implementados y testeados correctamente

--- src/main/java/com/agromaquinariasosorio/config/Test.java
+++ src/main/java/com/agromaquinariasosorio/config/Test.java
@@ -4,7 +4,11 @@
  */
 package com.agromaquinariasosorio.config;
 
+import com.agromaquinariasosorio.modelo.Categoria;
 import com.agromaquinariasosorio.modelo.CategoriaDAO;
+import com.agromaquinariasosorio.modelo.Producto;
+import com.agromaquinariasosorio.modelo.ProductoDAO;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.util.List;
 
@@ -13,15 +17,25 @@ import java.util.List;
  * @author MARY LEONOR
  */
 public class Test {
-      public static void main(String[] args) {
+      public static void main(String[] args) throws Exception {
         Conexion conexion = new Conexion();
         Connection con = conexion.getConnection();
 
         if (con != null) {
             System.out.println("Conexion exitosa a Oracle XE");
-            CategoriaDAO categoriaDAO = new CategoriaDAO();
-            List list = categoriaDAO.listar();
-            System.out.println(list.getFirst().toString());
+            ProductoDAO productoDAO = new ProductoDAO();
+            Producto pro = new Producto();
+            pro.setIdCategoria(new Categoria());
+            pro.getIdCategoria().setIdCategoria(1);
+            pro.setNombre("Motosierra Juanaza");
+            pro.setDescripcion("Muy buena y barata");
+            pro.setPrecio(new BigDecimal(460.40));
+            pro.setImagen("http://localhost:8080/imagen");
+            pro.setStock(45);
+            pro.setIdProducto(23);
+            
+            productoDAO.actualizar(pro);
+            //System.out.println(prod.toString());
         } else {
             System.out.println("Error al conectar a Oracle XE");
         }
